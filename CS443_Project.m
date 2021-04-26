4adc403dc99eb014da4b3fb3cd7110dbeb8f5809
NathanUlmer <44823422+NathanUlmer@users.noreply.github.com>
2021-04-26 18:40:30 -0500

    Initial Commit

---
+++ CS443_Project.m
@@ -0,0 +1,33 @@
+% CS443 Project
+% Group Members:
+%   Nathan Ulmer
+%   Dian Hernandez
+%   Juley Yeager
+%   Jared Nicoll
+%   Dalton Johnson
+%
+% Spring 2021
+clear all; close all; clc;
+
+
+
+
+%% Project Description
+% The goal of this project is to imlement the lossy compression part of the
+% JPEG algorithm. It implements and applies the specified JPEG algorithm
+% for the two included images in Project_Code_Images. 
+
+%% Step 1: Compression
+
+
+
+
+%% Step 2: Decompression
+
+%% Step 3: Outputs
+
+%% Step 4: Error Computation
+
+
+%% Step 5: Group Report Output
+
