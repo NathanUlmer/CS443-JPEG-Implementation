ac7b455b4ac3af50c0e6da4f1de430754acc8100
NathanUlmer <44823422+NathanUlmer@users.noreply.github.com>
2021-04-26 19:51:39 -0500

    Added empty functions for each step of the project

---
+++ IDCT2D.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = IDCT2D(inputArg1,inputArg2)
+%IDCT2D Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
